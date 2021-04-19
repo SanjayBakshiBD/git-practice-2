c8e995fcc6c5db78f1f7ffec8a926d88c1d591d6
Sanjay Bakshi <sanjaybakshi647@gmail.com>
2021-04-19 10:05:57 +0600

    here we convert from small letter to capital letters

--- array.c
+++ array.c
@@ -2,8 +2,28 @@
 int main()
 {
     char array[100];
+    int i;
+
     gets(array);
 
-    puts(array);
+    for(i=0; array[i] != '\0'; i++)
+    {
+        if((array[i] >='A') &&(array[i] <= 'Z'))
+        {
+            printf("%c", array[i]);
+        }
+        else if((array[i] >='a') &&(array[i] <= 'z'))
+        {
+            char x = (char)((int)array[i] - 32);
+            printf("%c", x);
+        }
+        else
+        {
+            printf("%c", array[i]);
+        }
+
+    }
+    printf("\n");
+
     return 0;
 }
